2b102b004354485f64b8c0adf7c9292094746fbd
Ali Shaharyar <alishaharyar@pm.me>
2023-11-24 19:03:30 +0500

    Updated dino.ino

--- ChromeDino/dino.ino
+++ ChromeDino/dino.ino
@@ -1,66 +1,40 @@
+//    CHROME DINO 
+
+// For this code, the LDR used gives a digital output.
+
+//Including the prebuilt library to control the servos
 #include <Servo.h>
 
+
+//Defining the pins for the LDR ; Change it to whatever pin yours is connected to
 #define ldr 3
-#define ldr2 4
-#define ldr3 5
+
+//Defining the servo 
 Servo serv;
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  serv.attach(9);                      
+
+  serv.attach(9);                     
   pinMode(ldr, INPUT);
-  Serial.begin(9600);
+  Serial.begin(9600); //Set the baudrate
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int data = digitalRead(ldr);
-  int d2 = digitalRead(ldr2);
-  int d3 = digitalRead(ldr3);
 
 
-  Serial.println(data);
-  if(d3 = 0){
+  int data = digitalRead(ldr); //Take input from the LDR
+
   if (data == 1){
-    serv.write(50);
+    serv.write(50); //Move servo if a change is detected
     delay(10);
     serv.write(0);
   }
   if (data == 0){                                      
-    serv.write(0);  
+    serv.write(0);  //Return to original postion if the change reverts
   }
   
-  if (d2 == 1){
-    serv.write(50);
-    delay(10);
-    serv.write(0);
-  }
-  if (d2 == 0){                                      
-    serv.write(0);  
-  }
-  }
-  
-  if(d3 = 1){
-  if (data == 0){
-    serv.write(50);
-    delay(10);
-    serv.write(0);
-  }
-  if (data == 1){                                      
-    serv.write(0);  
-  }
-  
-  if (d2 == 0){
-    serv.write(50);
-    delay(10);
-    serv.write(0);
-  }
-  if (d2 == 1){                                      
-    serv.write(0);  
-  }
-  }
-
-
 }
